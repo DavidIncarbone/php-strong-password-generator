71030e5a9ae843da4fc0bb8e662c027111f0a5ac
DavidIncarbone <david.incarbone@gmail.com>
2025-03-14 10:13:29 +0100

    add new fields into form

--- index.php
+++ index.php
@@ -6,16 +6,7 @@ require_once "./functions.php";
 
 $_SESSION["passwordGenerated"] = generatePassword($passwordLength);
 
-// if (!isset($passwordLength) || $passwordLength == 0) {
-
-//     header("Location: ./index.php");
-// } else {
-//     header("Location: ./result.php");
-// }
-
-var_dump($_SESSION);
-
-
+// var_dump($_SESSION);
 
 ?>
 <!DOCTYPE html>
@@ -25,6 +16,7 @@ var_dump($_SESSION);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="./style/style.css">
 
     <title>Strong password generator</title>
 </head>
@@ -32,16 +24,34 @@ var_dump($_SESSION);
 <body class="p-3">
 
     <h1 class="text-center">Strong password generator</h1>
-    <h2>Genera una password sicura</h2>
+    <h2 class="text-center text-white">Genera una password sicura</h2>
+    <div id="alert-info" class="p-3 my-4 d-none">Nessun parametro valido inserito</div>
 
-    <form action="result.php" method="GET">
+    <form action="result.php" method="GET" class="p-3">
 
-        <label for="passwordLength">
+        <div class="d-flex justify-content-between mb-3">
+            <label for="passwordLength"> Lunghezza password:</label>
+            <input type="number" id="passwordLength" name="passwordLength" min="5" max="20" required>
+        </div>
+        <div class="d-flex justify-content-between mb-3">
+            <label for="duplicates"> Consenti ripetizioni di uno o più caratteri:</label>
+            <div class="d-flex flex-column me-5">
+                <div><input type="radio" id="duplicates" name="duplicates" checked> <label for="duplicates">Sì</label></div>
+                <div><input type="radio" id="duplicates" name="duplicates"> <label for="duplicates">No</label></div>
 
-            Lunghezza password:
-
-        </label>
-        <input type="number" id="passwordLength" name="passwordLength" min="5" max="20" required>
+            </div>
+        </div>
+        <div class="d-flex flex-column align-items-end mb-3">
+            <div><input type="checkbox" id="letters" name="letters">
+                <label for="letters">Lettere</label>
+            </div>
+            <div><input type="checkbox" id="numbers" name="numbers">
+                <label for="numbers">Numeri</label>
+            </div>
+            <div><input type="checkbox" id="symbols" name="symbols">
+                <label for="symbols">Simboli</label>
+            </div>
+        </div>
         <button type="submit" class="btn btn-primary">Invia</button>
         <button type="reset" class="btn btn-secondary">Annulla</button>
 
